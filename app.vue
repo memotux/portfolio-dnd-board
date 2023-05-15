20c4dafeae4e2356033defd92637935e368b4da6
memoTux <romeo@mendezfuentes.net>
2023-05-15 16:06:14 -0600

    update: board header style class

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-[100vh] bg-teal-600 overflow-hidden">
-    <header class="flex items-center mb-10 bg-teal-700 rounded p-2 w-full">
+    <header class="flex items-center mb-10 bg-teal-700 p-4 w-full">
       <img width="200" height="200" class="mr-3 w-[50px] h-full" src="/vueschool-logo.svg"
         alt="Vue School" />
       <h1 class="text-4xl text-white ">DnD Board</h1>
