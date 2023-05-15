92c597f933d383097e7f092757d05d20be6207ef
memoTux <romeo@mendezfuentes.net>
2023-05-15 15:36:49 -0600

    add DnDBoard component

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <DnDBoard />
   </div>
 </template>
---
+++ components/DnDBoard.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { nanoid } from "nanoid";
+import type { Column } from '~/types';
+
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'Backlog',
+    tasks: [
+      {
+        id: nanoid(),
+        title: 'Create marketing landing page',
+        createdAt: new Date()
+      },
+      {
+        id: nanoid(),
+        title: 'Develop cool new feature',
+        createdAt: new Date()
+      },
+      {
+        id: nanoid(),
+        title: 'Fix page nav bug',
+        createdAt: new Date()
+      },
+    ]
+  },
+  { title: 'Selected for Dev', id: nanoid(), tasks: [] },
+  { title: 'In Progress', id: nanoid(), tasks: [] },
+  { title: 'QA', id: nanoid(), tasks: [] },
+  { title: 'Complete', id: nanoid(), tasks: [] },
+])
+</script>
+
+<template>
+  <div>
+    <div v-for="col in columns" :key="col.id">
+      <header>
+        {{ col.title }}
+      </header>
+      <p v-for="task in col.tasks" :key="task.id">{{ task.title }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
